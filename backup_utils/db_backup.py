029491b916c846c631e50a4809f6a257c4a56f07
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-02-01 11:09:48 +0100

    fix tar archive contents, remove default array values

--- backup_utils/db_backup.py
+++ backup_utils/db_backup.py
@@ -7,8 +7,8 @@ import glob
 
 DB_USER = 'backup'
 DB_PW = db_backup_pwd
-DB_NAMES = ['mysql','troznov','d178318_loupo']
-BACKUP_PATH = '/home/pi/backup/db'
+DB_NAMES = [] #names of databases to backup
+BACKUP_PATH = '/home/user/backup/db' #path to folder to store backups
 today = datetime.date.today()
 
 for name in DB_NAMES:
@@ -16,7 +16,7 @@ for name in DB_NAMES:
 		os.mkdir(BACKUP_PATH)
 	dump = 'mysqldump --force -u ' + DB_USER + ' -p' + DB_PW + ' -B ' + name + ' > ' + BACKUP_PATH + '/' + str(today) + '_' + name+ '.sql'
 	os.system(dump)
-	tar = 'tar -czf ' + BACKUP_PATH + '/' + str(today) + '_' + name + '.tar.gz ' + BACKUP_PATH + '/' + str(today) + '_' + name + '.sql'
+	tar = 'tar -czf ' + str(today) + '_' + name + '.tar.gz ' + BACKUP_PATH + '/' + str(today) + '_' + name + '.sql'
 	os.system(tar)
 
 if glob.glob(BACKUP_PATH + '/' + str(today) + '_*.sql'):

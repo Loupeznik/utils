6fe0f02e55fa7f3436f647cd4d23e64da4aeaf29
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-02-21 22:12:10 +0100

    optimize archiving, backup path is now dynamic

--- backup_utils/db_backup.py
+++ backup_utils/db_backup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
-from pwds import db_backup_pwd
+from pwds import db_backup_pwd # file with stored credentials
 import os
 import datetime
 import glob
+import getpass
 
 DB_USER = 'backup'
 DB_PW = db_backup_pwd
-DB_NAMES = [] #names of databases to backup
-BACKUP_PATH = '/home/user/backup/db' #path to folder to store backups
+DB_NAMES = [] # names of databases to backup
+SCRIPT_USER = getpass.getuser()
+BACKUP_PATH = '/home/' + SCRIPT_USER + '/backup/db' # path to folder to store backups
 today = datetime.date.today()
 
 for name in DB_NAMES:
@@ -16,10 +18,11 @@ for name in DB_NAMES:
 		os.mkdir(BACKUP_PATH)
 	dump = 'mysqldump --force -u ' + DB_USER + ' -p' + DB_PW + ' -B ' + name + ' > ' + BACKUP_PATH + '/' + str(today) + '_' + name+ '.sql'
 	os.system(dump)
-	tar = 'tar -czf ' + str(today) + '_' + name + '.tar.gz ' + BACKUP_PATH + '/' + str(today) + '_' + name + '.sql'
+	os.chdir(BACKUP_PATH)
+	tar = 'tar -czf ' + str(today) + '_' + name + '.tar.gz ' + str(today) + '_' + name + '.sql'
 	os.system(tar)
 
 if glob.glob(BACKUP_PATH + '/' + str(today) + '_*.sql'):
 	for dumpfile in glob.glob(BACKUP_PATH + '/*.sql'):
 		os.remove(dumpfile)
-	print(str(today) + ' - Databáze úspěšně zálohovány v ' + BACKUP_PATH)
+	print('[DB BACKUP] ' + str(today) + ' Databases successfuly backed up at ' + BACKUP_PATH)

fed665f8ff3bb6a3a5bfb65ae7c6df9bc5553666
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-02-21 22:10:44 +0100

    add status descriptors, optimize archiving

--- backup_utils/backup_to_mega.sh
+++ backup_utils/backup_to_mega.sh
@@ -3,27 +3,36 @@
 mega-login username password #placeholder values
 folders="$(mega-ls)"
 cur_month="$(date +%Y-%m)"
-db_folder="/home/pi/backup/db"
-temp_folder="/home/pi/scripts/temp"
-free=""
+db_folder="/home/$USER/backup/db"
+temp_folder="/home/$USER/scripts/temp"
+stime=$(date +"%s")
+echo "[MEGA BACKUP] Starting backup script"
 if [[ $folders == *$cur_month* ]]; then
 	mega-logout 1>/dev/null
-	echo MEGA BACKUP -- Záloha neúspěšná (záloha pro tento měsíc již existuje)
+	echo "[MEGA BACKUP] Backup not sucessful (backup for this month already exists)"
 	exit 0
 else
+	[ ! -d "$temp_folder" ] && mkdir -p "$temp_folder"
 	mega-mkdir "$cur_month"
+	echo "[MEGA BACKUP] Backing up database dumps"
 	for file in "$db_folder"/*; do
 		if [[ $file == *$cur_month* ]]; then
 			mega-put -q "$file" /"$cur_month"/
 		fi
 	done
-	tar -czf "$temp_folder"/scripts.tar.gz /home/pi/scripts 2>/dev/null
+	echo "[MEGA BACKUP] Backing up scripts directory"
+	tar -czf "$temp_folder"/scripts.tar.gz -C /home/$USER/scripts/ *.sh *.py *.rb *.go 2>/dev/null
 	mega-put -q "$temp_folder"/scripts.tar.gz /"$cur_month"/
 	# z nějakého kvalitního důvodu nefunguje -q přepínač vždy na posledním mega-put příkazu #
 	# vyřešeno přesměrováním všeho outputu do /dev/null 				        #
-	tar -czf "$temp_folder"/www.tar.gz /var/www/ 2>/dev/null
+	echo "[MEGA BACKUP] Backing up www directory"
+	tar -czf "$temp_folder"/www.tar.gz -C /var/ www 2>/dev/null
 	mega-put "$temp_folder"/www.tar.gz /"$cur_month"/ &>/dev/null
+	echo "[MEGA BACKUP] Removing backup archives from local storage"
 	rm "$temp_folder"/*
-	echo MEGA BACKUP -- záloha úspěšně provedena pro "$cur_month"
+	echo "[MEGA BACKUP] Backup successful for $cur_month"
 fi
+etime=$(date +"%s")
+timediff=$(($etime-$stime))
+echo "[MEGA BACKUP] Script took $(($difftimelps / 60)) minutes and $(($difftimelps % 60)) seconds to finish"
 mega-logout 1>/dev/null
